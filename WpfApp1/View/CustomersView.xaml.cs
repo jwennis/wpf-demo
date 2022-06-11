21f2d2c47ddffb3db782f07d080d47c5829baaec
Joe Ennis <j.w.ennis@gmail.com>
2022-06-11 13:29:13 -0400

    implemented customer ViewModel

--- WpfApp1/View/CustomersView.xaml.cs
+++ WpfApp1/View/CustomersView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Data;
+using WpfApp1.ViewModel;
 
 namespace WpfApp1.View
 {
@@ -20,9 +22,21 @@ namespace WpfApp1.View
     /// </summary>
     public partial class CustomersView : UserControl
     {
+        private CustomersViewModel _viewModel;
+
         public CustomersView()
         {
             InitializeComponent();
+
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+
+            DataContext = _viewModel;
+            Loaded += CustomersView_Loaded;
+        }
+
+        private async void CustomersView_Loaded(object sender, RoutedEventArgs e) 
+        {
+            await _viewModel.LoadAsync();
         }
 
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
---
+++ WpfApp1/ViewModel/CustomersViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Data;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class CustomersViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; } = new();
+
+        private readonly ICustomerDataProvider _customerDataProvider;
+
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+        }
+
+        public async Task LoadAsync()
+        {
+            if (Customers.Any()) 
+            {
+                return;
+            }
+
+            var customers = await _customerDataProvider.GetAllAsync();
+
+            if (customers is not null)
+            {
+                foreach (var customer in customers)
+                {
+                    Customers.Add(customer);
+                }
+            }
+
+
+        }
+    }
+}
